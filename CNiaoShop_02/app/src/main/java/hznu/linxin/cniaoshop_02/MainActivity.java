b0b41f0c2f6d494003e0df572d3614c96dc4c844
Bacsonlx <BacSonlx@163.com>
2021-03-05 22:37:18 +0800

    自定义ToolBar

---
+++ CNiaoShop_02/app/src/main/java/hznu/linxin/cniaoshop_02/MainActivity.java
@@ -0,0 +1,17 @@
+package hznu.linxin.cniaoshop_02;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * 自定义ToolBar
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
